72192a2a394411f81119c7f48750811038cd7e0e
Radoslav Petkov <radipirate@gmail.com>
2019-05-29 22:03:14 +0300

    change run because jvm opts too aggressive

--- java/run.sh
+++ java/run.sh
@@ -1,40 +1,40 @@
 echo "Starting pi-bench"
-java -jar build/libs/pi-bench-all.jar -mode single -precision 10000 -num_reps 3 -silent 
+java -jar build/libs/pi-bench-all.jar -mode single -precision 10000 -num_reps 1 -silent 
 echo "#######" 
-java -jar build/libs/pi-bench-all.jar -mode shared -precision 10000 -num_reps 3 -silent
+java -jar build/libs/pi-bench-all.jar -mode shared -precision 10000 -num_reps 1 -silent
 echo "#######"
 echo "Parallel threaded factorial"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 4 -precision 10000 -num_reps 3 -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 4 -precision 10000 -num_reps 1 -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 6 -precision 10000 -num_reps 3 -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 6 -precision 10000 -num_reps 1 -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 8 -precision 10000 -num_reps 3 -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 8 -precision 10000 -num_reps 1 -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 12 -precision 10000 -num_reps 3 -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 12 -precision 10000 -num_reps 1 -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 16 -precision 10000 -num_reps 3 -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 16 -precision 10000 -num_reps 1 -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 18 -precision 10000 -num_reps 3 -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 18 -precision 10000 -num_reps 1 -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 20 -precision 10000 -num_reps 3 -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 20 -precision 10000 -num_reps 1 -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 32 -precision 10000 -num_reps 3 -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 32 -precision 10000 -num_reps 1 -silent
 echo "#######"
 echo "Cached factorial"
-java -jar build/libs/pi-bench-all.jar -mode single -precision 10000 -num_reps 3 -fact_mode cached -silent
+java -jar build/libs/pi-bench-all.jar -mode single -precision 10000 -num_reps 1 -fact_mode cached -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 4 -precision 10000 -num_reps 3  -fact_mode cached -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 4 -precision 10000 -num_reps 1  -fact_mode cached -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 6 -precision 10000 -num_reps 3  -fact_mode cached -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 6 -precision 10000 -num_reps 1  -fact_mode cached -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 8 -precision 10000 -num_reps 3  -fact_mode cached -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 8 -precision 10000 -num_reps 1  -fact_mode cached -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 12 -precision 10000 -num_reps 3  -fact_mode cached -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 12 -precision 10000 -num_reps 1  -fact_mode cached -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 16 -precision 10000 -num_reps 3  -fact_mode cached -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 16 -precision 10000 -num_reps 1  -fact_mode cached -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 18 -precision 10000 -num_reps 3  -fact_mode cached -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 18 -precision 10000 -num_reps 1  -fact_mode cached -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 20 -precision 10000 -num_reps 3  -fact_mode cached -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 20 -precision 10000 -num_reps 1  -fact_mode cached -silent
 echo "#######"
-java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 32 -precision 10000 -num_reps 3  -fact_mode cached -silent
+java -jar build/libs/pi-bench-all.jar -mode parallel -num_threads 32 -precision 10000 -num_reps 1  -fact_mode cached -silent
