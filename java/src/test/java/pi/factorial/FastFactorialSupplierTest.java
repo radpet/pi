950d333555de9af2d7b19974ebee5baec3902f0c
Radoslav Petkov <radoslavp@starsgroup.com>
2019-06-02 22:27:11 +0300

    add sample execution time

--- java/src/test/java/pi/factorial/FastFactorialSupplierTest.java
+++ java/src/test/java/pi/factorial/FastFactorialSupplierTest.java
@@ -13,22 +13,22 @@ public class FastFactorialSupplierTest {
     @Test
     public void testWithoutParallel() {
         runTest(new DummyFactorialSupplier());
-    }
+    } //2s 403
 
     @Test
     public void testParallelismWorkStealing1() {
         runTest(new FastFactorialSupplier(Executors.newWorkStealingPool(1)));
-    }
+    } //1s 43
 
     @Test
     public void testParallelismForkJoin1() {
         runTest(new FastFactorialSupplier(new ForkJoinPool(1)));
-    }
+    } // 711ms
 
     @Test
     public void testExecutorForkJoin() {
         runTest(new FastFactorialSupplier(new ForkJoinPool()));
-    }
+    } //85ms
 
     private void runTest(FactorialSupplier factorialSupplier) {
         BigInteger result = factorialSupplier.get(100000);
